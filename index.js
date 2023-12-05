f4fcb59d579fe9a147ad7aec40313eddcba7790f
PriyankaaPatil12 <PriyankaaPatil12>
2023-12-05 14:43:38 +0530

    changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 var corsOptions = {
   origin:
-    "https://playful-gaufre-e218b0.netlify.app",
+    "https://priyankaregistrationform.netlify.app",
   methods: ["GET", "POST"], // or other HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // or other headers
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -39,6 +39,7 @@ app.listen(PORT, () => {
       .catch(() => {
         console.log("could not connect to mongodb");
       });
+      
   }
   main();
 
